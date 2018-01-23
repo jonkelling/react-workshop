49d0af23d74d1fff63f697f92cadd32964db1f8c
Mark Johnston <Mark.Johnston@sscgp.com>
2018-01-23 09:48:30 -0600

    Initial creation of some components.

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Layout extends React.Component {
+	render() {
+		return <div>
+			<div><h1>Team 2 Header</h1></div>
+			<div>Menu</div>
+			<div>Content</div>
+			<div>Footer</div>
+		</div>;
+	}
+}
