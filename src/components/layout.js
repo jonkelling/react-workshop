58b6dae121b815283a6fc5d518fc0952841286af
Abshier <Abshier_c@sscgp.com>
2018-01-23 10:50:00 -0600

    Menu Links added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Menu from './menu';
 
 export default class Layout extends React.Component {
 	render() {
 		return <div>
 			<div><h1>Team 2 Header</h1></div>
-			<div>Menu</div>
+			<Menu />
 			<div>Content</div>
 			<div>Footer</div>
 		</div>;
---
+++ src/components/menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Menu extends React.Component {
+    render(){
+        return <div> 
+                <a href = "#">Talks</a>
+                <a href = "#">About</a>
+                <a href = "#">Teds</a>
+        </div>
+        
+    }
+}
