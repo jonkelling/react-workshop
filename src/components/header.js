df4b8b2e6622828482e0d9ca050e600a9af7548b
Sharon Rice <Rice_s@sscgp.com>
2018-01-23 10:45:44 -0600

    Good looking header

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+	render() {
+		return <div>
+            <h1>UFOs Unleashed</h1>
+            <h2>Hide Your Anime</h2> 
+            <img src="https://i0.wp.com/psychics.co.uk/blog/wp-content/uploads/2013/03/ufo.jpg" width="100%" height="300" />            
+		</div>;
+	}
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Header from './header'
 export default class Layout extends React.Component{
 	render() {
 		return <div>
-			<div>Header</div>
-			<div>menu</div>
+			<Header />
+			<div>menu fluffy</div>
 			<div>Content</div>
 			<div>Footer</div>
 		</div>;
