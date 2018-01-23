b820f24a2c4346916ebde6dd2ba3e209b2fea7de
Joseph Payne <Payne_a@sscgp.com>
2018-01-23 09:48:42 -0600

    Initial Commit for UFO stuff

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Layout extends React.Component{
+	render() {
+		return <div>
+			<div>Header</div>
+			<div>menu</div>
+			<div>Content</div>
+			<div>Footer</div>
+		</div>;
+	}
+}
