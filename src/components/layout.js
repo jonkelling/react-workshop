32f7b6c69a027ad6a3ee27751f4c3dd511eeac8b
Trevor Ralph <34900064+Vanargand@users.noreply.github.com>
2018-01-23 10:53:40 -0600

    menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import Header from './header'
 import Footer from './footer'
 
@@ -8,6 +9,14 @@ export default class Layout extends React.Component{
 		return <div>
 			<Header />
 			<div>menu fluffy</div>
+=======
+import Menu from './menu.js';
+export default class Layout extends React.Component{
+	render() {
+		return <div>
+			<div>Header</div>
+			<Menu/>
+>>>>>>> Stashed changes
 			<div>Content</div>
 			<Footer />
 		</div>;
---
+++ src/components/menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Menu extends React.Component {
+    render() {
+        return <div>
+            <div align="left">
+                <div>
+                    <a href="">UFOs</a>
+                </div>
+                <div>
+                    <a href="">Sightings Map</a>
+                </div>
+            </div>
+        </div>;
+    }
+}
