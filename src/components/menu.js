80571e76f40c40e7049731afc6d838558a67d7aa
Abshier <Abshier_c@sscgp.com>
2018-01-23 13:42:16 -0600

    Buttons Are Links

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,9 +4,9 @@ import React from 'react';
 export default class Menu extends React.Component {
     render(){
         return<div>
-                    <button className ="App-button">Talks</button>
-                    <button className ="App-button">Catagories</button>
-                    <button className ="App-button">Teds</button>
+                    <a href= "#" className= "App-button">Talks</a>
+                    <a href= "#" className= "App-button">Catagories</a>
+                    <a href= "#" className= "App-button">TEDs</a>
                 </div>
                 
         
