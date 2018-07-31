5eab7435480fe544249e6b53cc8948065f48f939
Matthew Schladweiler <matthewschladweiler@gmail.com>
2018-07-31 09:27:57 -0500

    Started Content panel

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+// import './../App.css';
+
+import React from 'react';
+
+export default class Content extends React.Component {
+    render() {
+        return <div className="Content">
+
+        </div>;
+    }
+}
