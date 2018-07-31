c511f59551f4b8b1e7a7fea3da800a1092c19025
Liam Mueller <liam.mueller@centare.com>
2018-07-31 09:43:41 -0500

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import { Component, React } from 'react';
+
+class Footer extends Component
+{
+    render() {
+        return <footer>They're out there</footer>;
+    }
+}
+
+export default Footer;
