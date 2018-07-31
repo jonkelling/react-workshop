27e355785fb2b086d041ab0e5fb157662d03c036
Cody Lewis <cody01546@gmail.com>
2018-07-31 09:48:39 -0500

    add styling to content body

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react'
 export default class Content extends Component {
   render() {
       return (
-          <div>
-              <p>I am the content</p>
+          <div className='App-content-body'>
+              <p>I am the not the body</p>
           </div>
       )
   }
